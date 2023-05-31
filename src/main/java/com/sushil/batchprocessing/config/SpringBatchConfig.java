66bb5a3360fef2a23b260b5263052a974438f3aa
sushil <sushil.verma@walkingtree.tech>
2023-05-31 13:14:41 +0530

    added employee batch config with reader, lineMapper, processor, step and Job run method

---
+++ src/main/java/com/sushil/batchprocessing/config/SpringBatchConfig.java
@@ -0,0 +1,88 @@
+package com.sushil.batchprocessing.config;
+
+import com.sushil.batchprocessing.employee.Employee;
+import com.sushil.batchprocessing.employee.EmployeeRepository;
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
+import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.item.data.RepositoryItemWriter;
+import org.springframework.batch.item.file.FlatFileItemReader;
+import org.springframework.batch.item.file.LineMapper;
+import org.springframework.batch.item.file.mapping.BeanWrapperFieldSetMapper;
+import org.springframework.batch.item.file.mapping.DefaultLineMapper;
+import org.springframework.batch.item.file.transform.DelimitedLineTokenizer;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.FileSystemResource;
+
+@Configuration
+@EnableBatchProcessing
+public class SpringBatchConfig {
+
+    private JobBuilderFactory jobBuilderFactory;
+    private StepBuilderFactory stepBuilderFactory;
+    private EmployeeRepository employeeRepository;
+
+    @Autowired
+    public SpringBatchConfig(JobBuilderFactory jobBuilderFactory, StepBuilderFactory stepBuilderFactory, EmployeeRepository employeeRepository) {
+        this.jobBuilderFactory = jobBuilderFactory;
+        this.stepBuilderFactory = stepBuilderFactory;
+        this.employeeRepository = employeeRepository;
+    }
+
+    @Bean
+    public FlatFileItemReader<Employee> reader() {
+        FlatFileItemReader itemReader = new FlatFileItemReader();
+        itemReader.setResource(new FileSystemResource("src/main/resources/MOCK_DATA.csv"));
+        itemReader.setName("csvReader");
+        itemReader.setLinesToSkip(1);
+        itemReader.setLineMapper(lineMapper());
+        return itemReader;
+    }
+
+    private LineMapper<Employee> lineMapper() {
+        DefaultLineMapper<Employee> lineMapper = new DefaultLineMapper<>();
+        DelimitedLineTokenizer lineTokenizer = new DelimitedLineTokenizer();
+        lineTokenizer.setDelimiter(",");
+        lineTokenizer.setStrict(false);
+        lineTokenizer.setNames("id", "firstName", "lastName", "email", "gender");
+
+        BeanWrapperFieldSetMapper<Employee> fieldSetMapper = new BeanWrapperFieldSetMapper<>();
+        fieldSetMapper.setTargetType(Employee.class);
+        lineMapper.setLineTokenizer(lineTokenizer);
+        lineMapper.setFieldSetMapper(fieldSetMapper);
+        return lineMapper;
+    }
+
+    @Bean
+    public EmployeeProcessor processor() {
+        return new EmployeeProcessor();
+    }
+
+    @Bean
+    public RepositoryItemWriter<Employee> writer() {
+        RepositoryItemWriter<Employee> writer = new RepositoryItemWriter<>();
+        writer.setRepository(employeeRepository);
+        writer.setMethodName("save");
+        return writer;
+    }
+
+    @Bean
+    public Step step1() {
+        return stepBuilderFactory.get("csv-step").<Employee, Employee>chunk(20)
+                .reader(reader())
+                .processor(processor())
+                .writer(writer())
+                .build();
+    }
+
+    @Bean
+    public Job run() {
+        return jobBuilderFactory.get("importEmployee")
+                .flow(step1())
+                .end().build();
+    }
+}
