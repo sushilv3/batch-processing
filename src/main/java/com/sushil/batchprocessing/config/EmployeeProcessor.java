fe936d8eebcc3a8bc14a6a62598b2d4645e860ab
sushil <sushil.verma@walkingtree.tech>
2023-06-01 11:31:48 +0530

    added filter condition in employee process

--- src/main/java/com/sushil/batchprocessing/config/EmployeeProcessor.java
+++ src/main/java/com/sushil/batchprocessing/config/EmployeeProcessor.java
@@ -8,6 +8,12 @@ import org.springframework.context.annotation.Configuration;
 public class EmployeeProcessor implements ItemProcessor<Employee,Employee> {
     @Override
     public Employee process(Employee employee) throws Exception {
+       /*here we can write any business logic before writing data in db */
+   /*     if(employee.getGender().equalsIgnoreCase("Male")){
+            return employee;
+        }else{
+            return null;
+        }*/
         return employee;
     }
 }
